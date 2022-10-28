a032e99c63248256d3b9a918dca9ab3e149825fc
josias-r <josias.ribi@gmail.com>
2022-10-28 10:43:37 +0200

    fix: init demo page

--- src/main.ts
+++ src/main.ts
@@ -1,157 +1,394 @@
-import { Boarding } from "./lib/main";
-import Driver from "driver.js";
 import "./lib/boarding.css";
+import { BoardingStepDefinition } from "./lib/boarding-types";
+import { Boarding } from "./lib/main";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a id="vitesvg" href="https://vitejs.dev" target="_blank" style="margin: 10px; display: inline-block; position: relative;">
-      x
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+import "./demo/styles/styles.css";
 
-const history: any[] = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const tourSteps: BoardingStepDefinition[] = [
+    {
+      element: document.getElementById("boarding-demo-head") as HTMLElement,
+      popover: {
+        className: "scoped-boarding-popover",
+        title: "Before we start",
+        description:
+          "This is just one use-case, make sure to check out the rest of the docs below.",
+        nextBtnText: "Okay, Start!",
+      },
+    },
+    {
+      element: "#logo_img",
+      popover: {
+        title: "Focus Anything",
+        description:
+          "You can use it to highlight literally anything, images, text, div, span, li etc.",
+      },
+    },
+    {
+      element: "#name_boarding",
+      popover: {
+        title: "Why Boarding?",
+        description: "Because it lets you drive the user across the page",
+      },
+    },
+    {
+      element: "#boarding-demo-head",
+      popover: {
+        title: "Let's talk features",
+        description:
+          "You may leave your mouse and use the <strong>arrow keys</strong> to move next and back or <strong>escape key</strong> anytime to close this",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#highlight_feature",
+      popover: {
+        title: "Highlight Feature",
+        description:
+          "You may use it to highlight single elements (with or without popover) e.g. like facebook does while creating posts",
+      },
+    },
+    {
+      element: "#feature_introductions_feature",
+      popover: {
+        title: "Feature Introductions",
+        description:
+          "With it's powerful API you can use it to make programmatic or user driven feature introductions",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#focus_shifters_feature",
+      popover: {
+        title: "Focus Shifters",
+        description:
+          "If some element or part of the page needs user's interaction, you can just call the highlight method. Boarding will take care of driving the user there",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#customizable_feature",
+      popover: {
+        title: "Highly Customizable",
+        description:
+          "Boarding has a powerful API allowing you to customize the experience as much as you can.",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#keyboard_feature",
+      popover: {
+        title: "User Friendly",
+        description:
+          "Your users can control it with the arrow keys on keyboard, or escape to close it",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#free_use_feature",
+      popover: {
+        title: "MIT License",
+        description:
+          "I believe in open-source and thus Boarding is completely free for both personal or commercial use",
+      },
+    },
+    {
+      element: "#lightweight_feature",
+      popover: {
+        title: "Only ~4KB",
+        description:
+          "Boarding is free of bloat and written in Vanilla JS. There is no external dependency at all, thus keeping it smaller in size.",
+      },
+    },
+    {
+      element: "#examples_section",
+      popover: {
+        title: "Usage Examples",
+        description:
+          "Have a look at the usage examples and see how you can use it.",
+      },
+    },
+    {
+      element: "#boarding-demo-head",
+      popover: {
+        title: "Quick Tour Ends",
+        description:
+          "This was just a sneak peak, have a look at the API section and examples to learn more!",
+      },
+    },
+  ];
 
-const tour = new Boarding({
-  onHighlighted: (ele) => {
-    history.push(`HIGHLIGHTER: ${ele.highlightDomElement.innerText}`);
-  },
-  onDeselected: (ele) => {
-    history.push(`DELECTED: ${ele.highlightDomElement.innerText}`);
-  },
-  onHighlightStarted: (ele) => {
-    history.push(`HIGH STARTED: ${ele.highlightDomElement.innerText}`);
-  },
-  onNext: (ele) => {
-    history.push(`NEXTed: ${ele.highlightDomElement.innerText}`);
-  },
-  onPrevious: (ele) => {
-    history.push(`PREVIOUSed: ${ele.highlightDomElement.innerText}`);
-  },
-  onReset: (ele) => {
-    history.push(`RESETed: ${ele.highlightDomElement.innerText}`);
-  },
+  const animatedTourBoarding = new Boarding({
+    animate: true,
+    opacity: 0.8,
+    padding: 5,
+    showButtons: true,
+  });
+
+  const boringTourBoarding = new Boarding({
+    animate: false,
+    opacity: 0.8,
+    padding: 5,
+    showButtons: true,
+    className: "boring-scope",
+  });
+
+  boringTourBoarding.defineSteps(tourSteps);
+  animatedTourBoarding.defineSteps(tourSteps);
+
+  document.querySelector("#animated-tour")?.addEventListener("click", () => {
+    if (boringTourBoarding.isActivated) {
+      boringTourBoarding.reset(true);
+    }
+
+    animatedTourBoarding.start();
+  });
+
+  document.querySelector("#boring-tour")?.addEventListener("click", () => {
+    if (animatedTourBoarding.isActivated) {
+      animatedTourBoarding.reset(true);
+    }
+
+    boringTourBoarding.start();
+  });
+
+  try {
+    document.querySelectorAll("pre code").forEach((element) => {
+      hljs.highlightBlock(element);
+    });
+  } catch (e) {
+    // Silently ignore the highlight errors
+  }
+
+  /////////////////////////////////////////////
+  // First example – highlighting without popover
+  /////////////////////////////////////////////
+  const singleBoardingNoPopover = new Boarding();
+  document
+    .querySelector("#run-single-element-no-popover")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+      singleBoardingNoPopover.highlight("#single-element-no-popover");
+    });
+
+  /////////////////////////////////////////////
+  // Form focus examples
+  /////////////////////////////////////////////
+  const focusBoarding = new Boarding({ padding: 0 });
+  const inputIds = [
+    "creation-input",
+    "creation-input-2",
+    "creation-input-3",
+    "creation-input-4",
+  ];
+  inputIds.forEach((inputId) => {
+    // Highlight the section on focus
+    document.getElementById(inputId)?.addEventListener("focus", () => {
+      focusBoarding.highlight(`#${inputId}`);
+    });
+  });
+
+  /////////////////////////////////////////////
+  // Highlighting single element with popover
+  /////////////////////////////////////////////
+  const singleBoardingWithPopover = new Boarding();
+  document
+    .querySelector("#run-single-element-with-popover")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+      singleBoardingWithPopover.highlight({
+        element: "#single-element-with-popover",
+        showButtons: false,
+        popover: {
+          title: "Did you know?",
+          description: "You can add HTML in title or description also!",
+          position: "top",
+        },
+      });
+    });
+
+  /////////////////////////////////////////////////////
+  // Highlighting single element with popover position
+  /////////////////////////////////////////////////////
+  const singleBoardingWithPopoverPosition = new Boarding();
+  document
+    .querySelector("#run-single-element-with-popover-position")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      singleBoardingWithPopoverPosition.highlight({
+        element: "#single-element-with-popover-position",
+        showButtons: false,
+        popover: {
+          title: "Did you know?",
+          description: "You can add HTML in title or description also!",
+          position: "top",
+        },
+      });
+    });
+
+  /////////////////////////////////////////////////////
+  // Highlighting single element with popover position
+  /////////////////////////////////////////////////////
+  const positionBtnsBoarding = new Boarding({
+    padding: 0,
+  });
+
+  document.querySelector("#position-btns")?.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let id = (e.target as HTMLElement).id;
+    let alignment = (e.target as HTMLElement).dataset.alignment;
+
+    if (!alignment) return;
+
+    positionBtnsBoarding.highlight({
+      element: `#${id}`,
+      showButtons: false,
+      popover: {
+        title: "Did you know?",
+        description: "You can add HTML in title or description also!",
+        position: alignment,
+      },
+    });
+  });
+
+  /////////////////////////////////////////////////////
+  // Highlighting single element with popover position
+  /////////////////////////////////////////////////////
+  const htmlBoarding = new Boarding();
+
+  document
+    .querySelector("#run-single-element-with-popover-html")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      htmlBoarding.highlight({
+        element: "#single-element-with-popover-html",
+        showButtons: false,
+        popover: {
+          title: "<em>Tags</em> in title or <u>body</u>",
+          description: "Body can also have <strong>html tags</strong>!",
+          position: "top",
+        },
+      });
+    });
+
+  /////////////////////////////////////////////////////
+  // Without Overlay Example
+  /////////////////////////////////////////////////////
+  const withoutOverlay = new Boarding({
+    opacity: 0,
+    padding: 0,
+  });
+
+  document
+    .querySelector("#run-element-without-popover")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      withoutOverlay.highlight({
+        element: "#run-element-without-popover",
+        popover: {
+          title: "Title for the Popover",
+          description: "Description for it",
+          position: "top", // can be `top`, `left`, `right`, `bottom`
+        },
+      });
+    });
+
+  /////////////////////////////////////////////
+  // Single no close demo
+  /////////////////////////////////////////////
+  const singleNoClose = new Boarding({
+    allowClose: false,
+    position: "top",
+  });
+
+  singleNoClose.defineSteps([
+    {
+      element: "#single-element-no-close",
+      popover: {
+        title: "Uh-huh!",
+        description: "You cannot close by clicking outside",
+      },
+    },
+    {
+      element: "#third-element-introduction",
+      popover: {
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "top",
+      },
+    },
+  ]);
+
+  document
+    .querySelector("#run-single-element-no-close")
+    ?.addEventListener("click", function (e) {
+      e.preventDefault();
+      singleNoClose.start();
+    });
+
+  /////////////////////////////////////////////////////
+  // Highlighting single element with popover position
+  /////////////////////////////////////////////////////
+  const featureIntroductionBoarding = new Boarding();
+  featureIntroductionBoarding.defineSteps([
+    {
+      element: "#first-element-introduction",
+      popover: {
+        className: "first-step-popover-class",
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "top",
+      },
+    },
+    {
+      element: "#second-para-feature-introductions",
+      popover: {
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "bottom",
+      },
+    },
+    {
+      element: "#third-para-feature-introductions",
+      popover: {
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "top",
+      },
+    },
+    {
+      element: "#run-multi-element-popovers",
+      popover: {
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "top",
+      },
+    },
+    {
+      element: "#third-element-introduction",
+      popover: {
+        title: "Title on Popover",
+        description: "Body of the popover",
+        position: "top",
+      },
+    },
+  ]);
+
+  document
+    .querySelector("#run-multi-element-popovers")
+    ?.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      featureIntroductionBoarding.start();
+    });
+
+  const newURL = location.href.split("?")[0];
+  if (newURL !== location.href) {
+    window.location = newURL;
+    window.location.href = newURL;
+  }
 });
-tour.defineSteps([
-  {
-    element: "#vitesvg",
-    popover: { title: "Test", description: "description" },
-    onNext: () => {
-      history.push("ON NEXT firststep");
-    },
-    onPrevious: () => {
-      history.push("ON PREVIOUS firststep");
-    },
-  },
-  {
-    element: "h1",
-    popover: { title: "Test2", description: "description2" },
-    onNext: () => {
-      history.push("ON NEXT secondstep");
-    },
-    onPrevious: () => {
-      history.push("ON PREVIOUS secondstep");
-    },
-  },
-  {
-    element: ".read-the-docs",
-    popover: { title: "Test3", description: "description3" },
-    onNext: () => {
-      history.push("ON NEXT thirdstep");
-    },
-    onPrevious: () => {
-      history.push("ON PREVIOUS thirdstep");
-    },
-  },
-]);
-
-tour.start();
-tour.moveNext();
-tour.movePrevious();
-tour.moveNext();
-tour.moveNext();
-tour.moveNext();
-
-// DRIVER JS COMPARISON
-
-const history2: any[] = [];
-
-const driverTour = new Driver({
-  onHighlighted: (ele) => {
-    history2.push(`HIGHLIGHTER: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-  onDeselected: (ele) => {
-    history2.push(`DELECTED: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-  onHighlightStarted: (ele) => {
-    history2.push(`HIGH STARTED: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-  onNext: (ele) => {
-    history2.push(`NEXTed: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-  onPrevious: (ele) => {
-    history2.push(`PREVIOUSed: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-  onReset: (ele) => {
-    history2.push(`RESETed: ${(ele.getNode() as HTMLElement).innerText}`);
-  },
-});
-driverTour.defineSteps([
-  {
-    element: "#vitesvg",
-    popover: { title: "Test", description: "description" },
-    onNext: () => {
-      history2.push("ON NEXT firststep");
-    },
-    onPrevious: () => {
-      history2.push("ON PREVIOUS firststep");
-    },
-  },
-  {
-    element: "h1",
-    popover: { title: "Test2", description: "description2" },
-    onNext: () => {
-      history2.push("ON NEXT secondstep");
-    },
-    onPrevious: () => {
-      history2.push("ON PREVIOUS secondstep");
-    },
-  },
-  {
-    element: ".read-the-docs",
-    popover: { title: "Test3", description: "description3" },
-    onNext: () => {
-      history2.push("ON NEXT thirdstep");
-    },
-    onPrevious: () => {
-      history2.push("ON PREVIOUS thirdstep");
-    },
-  },
-]);
-
-driverTour.start();
-driverTour.moveNext();
-driverTour.movePrevious();
-driverTour.moveNext();
-driverTour.moveNext();
-driverTour.moveNext();
-
-// console.log(history, history2);
-
-if (history.join() === history2.join()) {
-  console.log("HISTORY: ITS  THE SAME");
-} else {
-  console.error("HISTORY: ITS NOT THE SAME!!!!");
-}
-
-// console.log(history.join());
-// console.log(history2.join());
-
-tour.start();
