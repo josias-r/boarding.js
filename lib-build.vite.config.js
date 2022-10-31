53cb8c8f5c0007775ae1da82781172d6803ecb8f
josias-r <josias.ribi@gmail.com>
2022-10-31 15:19:21 +0100

    use seperate vite config for lib vs gh-page build

--- lib-build.vite.config.js
+++ lib-build.vite.config.js
@@ -1,12 +1,13 @@
 import path from "path";
 import { defineConfig } from "vite";
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/lib/main.ts"),
       name: "boarding.js",
-      fileName: "boarding",
+      fileName: "main",
     },
   },
 });
